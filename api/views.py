15f93bafbcc3dbe133dcf29643aadbab71f70361
GloryKO <koladeg2022@gmail.com>
2023-02-19 08:47:30 -0800

    added view for update and delete

--- api/views.py
+++ api/views.py
@@ -7,8 +7,8 @@ from .serializers import TaskSerializer
 
 @api_view(['GET'])
 def get_task_list(request):
-    task = Task.objects.all()
-    serializer =TaskSerializer(task,Many=True)
+    tasks = Task.objects.all()
+    serializer =TaskSerializer(tasks,Many=True)
     return Response(serializer.data,status=status.HTTP_200_OK)
 
 @api_view(['GET'])
@@ -24,6 +24,24 @@ def create_task(request):
         serializer.save()
         return Response(serializer.data,status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def  update_task(request,pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task,data=request.data)
+    if serializer.is_valid():
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_task(request,pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response("Deleted Successfully",status=status.HTTP_202_ACCEPTED)
     
+
+
+
+
 
 
