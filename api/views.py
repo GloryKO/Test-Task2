9d8fb788f7aa353418644be0dc09a59732d9ed39
GloryKO <koladeg2022@gmail.com>
2023-02-19 08:35:07 -0800

    added view to create task

--- api/views.py
+++ api/views.py
@@ -16,3 +16,14 @@ def get_single_task_details(request,pk):
     task = Task.objects.get(id=pk)
     serializer = TaskSerializer(task,Many=False)
     return Response(serializer.data,status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def create_task(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.data.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+
